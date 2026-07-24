0896cfba242d50e6e821d54ec890749120bac6cf
Emerance63 <iribagizaemerance63@gmail.com>
2026-07-24 19:46:21 +0200

    render fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'export', // 1. Tells Next.js to create the 'out' folder for Render
   images: {
+    unoptimized: true, // 2. Required so your Unsplash images don't crash the static build
     remotePatterns: [
       {
         protocol: "https",
